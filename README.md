0c6eecf815c01d660b56e09737def63215147dc8
Piotrek Budzowski <piobudzows@gmail.com>
2025-02-16 09:02:19 +0100

    Upload the Usuario

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # CursoJava-ValidarContrasenas
- Crear un sistema que gestione usuarios y valide contraseñas bajo múltiples reglas.
+Crear un sistema que gestione usuarios y valide contraseñas bajo múltiples reglas.
+    - Diseña una clase Usuario con atributos nombreUsuario, contraseña y un array de tres para almacenar las ultimas tres contraseñas. Este array de primeras comenzará con todas sus posiciones vacías y se irá rellenando, implementa la lógica necesaria para que cuando se almacene la cuarta contraseña se borre la primera y la segunda pase a estar en primer lugar.
+    - Implementa métodos para:
+    - Validar que la contraseña tenga al menos 8 caracteres, incluya una letra mayúscula, un número y un carácter especial.
+    - Cambiar la contraseña, validando que la nueva contraseña no sea igual a las últimas 3 usadas
+    - Crea una clase Administrador que extienda Usuario y permita reiniciar contraseñas de cualquier usuario a una contraseña predeterminada.
+    - En el main, permite al usuario:
+    - Registrar nuevos usuarios.
+    - Iniciar sesión, validando la contraseña.
+    - Cambiar contraseñas según las reglas.
+    - Crea un método en el main para precargar datos de forma automática al comenzar el proyecto y que las pruebas sean más rápidas y eficientes.
---
+++ Usuario.java
@@ -0,0 +1,97 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Usuario {
+
+	// CLASE USUARIO CON SUS TRES VARIABLES
+	private String nombreUsuario;
+	private String contrasena;
+	private String[] tresContrasenas;
+
+	// CREO UN CONSTRUCTOR QUE PEDIRA SOLAMENTE EL NOMBRE DE USUARIO, Y EJECUTARA METODO DE PEDIR
+	// CONTRASEÑA QUE DESAROLLARE EN SETCONTRASENA PARA PODER UTILIZARLO DE NUEVO EN MOMENTO DE CAMBIAR 
+	// LA CONTRASEÑA.
+	
+
+	public Usuario(String nombreUsuario) {
+		this.nombreUsuario = nombreUsuario;
+		this.tresContrasenas = new String[3];
+		setContrasena();
+	}
+
+	public String getNombreUsuario() {
+		return nombreUsuario;
+	}
+
+	public void setNombreUsuario(String nombreUsuario) {
+		this.nombreUsuario = nombreUsuario;
+	}
+
+	public String getContrasena() {
+		return contrasena;
+	}
+
+	// DESAROLLO UN METODO QUE PRIMERO CON UNA EXPRESION REGULAR COMPROBARA SI LA CONTRASEÑA TIENE
+	// EN ALGUN LADO AL MENOS UN CARACTER EN MAYUSCULA, EN ALGUN LADO AL MENOS UN NUMERO, EN ALGUN
+	// PARTE UN SIGNO RARO, Y AL MENOS 8 CARACTERES, POSTERIORMENTE CREO UN BUCLE QUE SI NO SE CUMPLE 
+	// O ESTA REPETIDA, EN LAS TRES ANTERIORES CONTRASEÑAS VALIDAS, VOLVERA A PEDIR UNA Y OTRA VEZ
+	// PARA AQUELLO CREO DOS METODOS MAS.
+	
+	public void setContrasena() {
+		Scanner sn = new Scanner(System.in);
+		String nuevaContrasena;
+		String expresion = "^(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$";
+		Pattern p = Pattern.compile(expresion);
+		boolean seguro = true;
+
+		while (seguro) {
+			System.out.print(
+					"Escribe una contraseña, al menos 8 caracteres, incluya una letra mayuscula, un numero y un caracter especial = [@ $ ! % * ? &]");
+			nuevaContrasena = sn.nextLine();
+			Matcher m = p.matcher(nuevaContrasena);
+
+			if (m.matches() && contrasenaRepetida(nuevaContrasena)) {
+				this.contrasena = nuevaContrasena;
+				actualizarTresContrasenas(nuevaContrasena);
+				System.out.println("Contraseña guardada.");
+				seguro = false;
+			} else {
+				System.err.println(
+						"Error: La contraseña debe tener al menos 8 caracteres, una MAYUSCULA, un numero y un caracter especial.");
+			}
+		}
+	}
+
+	public String[] getTresContrasenas() {
+		return tresContrasenas;
+	}
+
+	public void setTresContrasenas(String[] tresContrasenas) {
+		this.tresContrasenas = tresContrasenas;
+	}
+	
+	// PRIMERO DE ELLOS ES SOLO PARA RECORER ARRAY DE TRES ULTIMAS CONTRASEÑAS Y SI ENCUENTRA QUE ESTA 
+	// IGUAL DEVOLVERA FALLO DE OPERACION.
+	
+	private boolean contrasenaRepetida(String nuevaContrasena) {
+		boolean noEncontrado = true;
+		for (int i = 0; i < tresContrasenas.length; i++) {
+			if (tresContrasenas[i] != null && tresContrasenas[i].equals(nuevaContrasena)) {
+				noEncontrado = false;
+			}
+		}
+		return noEncontrado;
+	}
+	
+	// SEGUNDO PARA GUARDAR LA CONTRASEÑA NUEVA EN PUESTO 0 Y CAMBIAR DE POSISION OTRAS PERDIENDO SIEMPRE 
+	// LA TERCERA EN CASO DE QUE HUBIERA CAMBIADO Y LLEGADO A 4 CONTRASEÑAS EN LA APP.
+	
+	private void actualizarTresContrasenas(String nuevaContrasena) {
+		for (int i = tresContrasenas.length - 1; i > 0; i--) {
+			tresContrasenas[i] = tresContrasenas[i - 1];
+		}
+		tresContrasenas[0] = nuevaContrasena;
+	}
+
+}
